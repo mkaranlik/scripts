a27fa44f6eb5b28d03b39820fd9fe64f565998c4
Muhammet KARANLIK <ab255640@adalet.gov.tr>
2022-11-18 12:58:32 +0300

    etcd script edited

--- etcdinstall.sh
+++ etcdinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo yum -y install curl wget vim
 ETCD_RELEASE=$(curl -s https://api.github.com/repos/etcd-io/etcd/releases/latest|grep tag_name | cut -d '"' -f 4)
 wget https://github.com/etcd-io/etcd/releases/download/${ETCD_RELEASE}/etcd-${ETCD_RELEASE}-linux-amd64.tar.gz
 tar xvf etcd-${ETCD_RELEASE}-linux-amd64.tar.gz
