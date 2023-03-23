74e8e971957947049ff3db1e55baa6e76ef75535
mkaranlik <46562979+mkaranlik@users.noreply.github.com>
2023-03-23 11:57:14 +0300

    Update nexusinstall.sh

--- nexusinstall.sh
+++ nexusinstall.sh
@@ -3,5 +3,5 @@ mkdir -p /$HOME/nexus
 cd /$HOME/nexus
 wget https://download.sonatype.com/nexus/3/nexus-3.42.0-01-unix.tar.gz
 tar xzvf nexus-3.42.0-01-unix.tar.gz
-cd /tmp/nexus/nexus-3.42.0-01/bin
+cd /$HOME/nexus/nexus-3.42.0-01/bin
 ./nexus start
